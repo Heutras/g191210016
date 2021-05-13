f5976244ad7f6e97151cac25a34610a482bd17e0
Heutras <alperceltr@gmail.com>
2021-05-13 12:08:45 +0300

    Add files via upload

---
+++ v8/login/index.php
@@ -0,0 +1,29 @@
+<html>
+
+<?php 
+$mail = "g191210016@gmail.com";
+$password = "1";
+$temp_mail = $_POST['mail'];
+$temp_password = $_POST['password'];
+
+alert("Hello World");
+
+if ($temp_mail == $mail && $temp_password == $password){
+  alert("Başarıyla Giriş Yaptınız, Alper Çelik.");
+  header('refresh: 0; url=../index.html');
+
+}
+elseif ($temp_mail != $mail){
+  alert("Girdiğiniz Mail Hatalı !");
+  header('refresh: 0; url=index.html');
+
+
+}elseif ($temp_password != $password){
+  alert("Girdiğiniz Şifre Hatalı !");
+  header('refresh: 0; url=index.html');
+}
+
+function alert($msg) {
+  echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+?>
