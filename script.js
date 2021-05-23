7082c78951905ef44eb5431511855d40ed9700b8
Heutras <alperceltr@gmail.com>
2021-05-23 18:53:53 +0300

    Şehrim'in bitişi
    Şehrim sayfasında navbar'ın pozisyonuyla alakalı sorunu çözdüm ve şehrim'i son haline getirdim.

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const container = document.querySelector('.container');
 open.addEventListener('click' , () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://api.collectapi.com/imdb/imdbSearchByName?query=inception");
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("authorization", "apikey 7eKXqq3eRVaO5hCYbiLHEv:2R3Vi8iiFxDEjgZJlQfPbX");
+
+xhr.send(data);
